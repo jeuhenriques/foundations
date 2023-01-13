5153be056cea3d5d275755618a0a78369ad63821
jeuhenriques <joao.e.henriques@nos.pt>
2023-01-13 14:07:59 +0000

    update cleaning

--- assignment_1/life_expectancy/cleaning.py
+++ assignment_1/life_expectancy/cleaning.py
@@ -1,20 +1,17 @@
 """Module clean data"""
 import pandas as pd
 import argparse
+import pathlib
 
-def clean_data(region="PT",
-               path="C:\\repos\\faast-foundations\\assignments\\life_expectancy\\data\\",
-               file_input_name="eu_life_expectancy_raw.tsv",
-               file_output_name="pt_life_expectancy.csv") -> None:
+def clean_data(region="PT") -> None:
     """
-
     :param region:
-    :param path:
-    :param file_input_name:
-    :param file_output_name:
     """
+    path = pathlib.Path(__file__).parent / 'data'
+    file_input_name="eu_life_expectancy_raw.tsv"
+    file_output_name="pt_life_expectancy.csv"
     # read data
-    data = pd.read_csv(path+file_input_name, sep='\t')
+    data = pd.read_csv(path/file_input_name, sep='\t')
     # slice data frame column to split
     split_column = data.columns[0]
     # slip column by comma
@@ -36,7 +33,9 @@ def clean_data(region="PT",
     # Filter region column by region parameter
     data = data[data["region"] == region]
     # save data
-    data.to_csv(path + file_output_name, index=False)
+    data.to_csv(path/file_output_name, index=False)
+
+    return None
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
